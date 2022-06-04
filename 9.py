cefddf532e2a3048d674fcf556ac09da7e0542eb
V3il3DOraCl3 <javadaaghajanloo@gmail.com>
2022-06-04 00:59:20 -0700

    Initial commit

---
+++ 9.py
@@ -0,0 +1,6 @@
+"""
+9. Develop a Circle class that, like the Rectangle class above, provides methods to compute perimeter
+and area. The Rectangle instance variables are not appropriate for circles; specifically, circles do
+not have corners, and there is no need to specify a width and height. A center point and a radius more
+naturally describe a circle. Build your Circle class appropriately.
+"""
