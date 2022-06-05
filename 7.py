0331daa31182383a90f2ee1b32528bec5af1e801
V3il3DOraCl3 <javadaaghajanloo@gmail.com>
2022-06-05 03:51:31 -0700

    initial commit

--- 7.py
+++ 7.py
@@ -1,3 +1,20 @@
 """
 7. Given the definition of the geometric Point class, add a method named distance:
 """
+import math
+
+
+class Point:
+
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+    def doubledistance(self, p):
+        d = math.sqrt((a.x - p[0]) ** 2 + (a.y - p[1]) ** 2)
+        return d
+
+
+if __name__ == '__main__':
+    a = Point(2, 3)
+    print(a.doubledistance((3, 3)))
